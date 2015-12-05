663e7ad8041ceb1072139ee674773e90faad5f31
Arthur Colle <hello@arthurcolle.com>
2015-12-04 23:20:28 -0500

    Fixing style

--- lib/graph.ex
+++ lib/graph.ex
@@ -1,134 +1,48 @@
 defmodule Graph do
-	
-	###################################################################
-	# (private) 
-	#
-	# init/0  -  initializes the :digraph from otp, 
-	#				     holds its state with an agent
 
-	defp init do
-		Agent.start_link(fn -> :digraph.new end)
-	end
+  ############ Private functions ##############################
 
+  defp init do
+    Agent.start_link(fn -> :digraph.new end)
+  end
+  
+  ############ Public functions ###############################
 
-	###################################################################
-	# (public)
-	#
-	# new/0   - client creates an Agent holding a :digraph
-	#
-	# notes - use when you need to catch errors, e.g. by 
-	#					matching on `{response_code, pid} = Graph.new`
+  # new/0   - client creates an Agent holding a :digraph
+  # 
+  # notes - use when you need to catch errors, e.g. by 
+  #					matching on `{response_code, pid} = Graph.new`
 
-	def new do
-		case init() do
-			{:ok, pid} -> 
-				{:ok, pid}
-			{:error, error} -> 
-				{:error, error}
+  def new do
+    case init() do
+      {:ok, pid} -> 
+        {:ok, pid}
+      {:error, error} -> 
+        {:error, error}
 			anything_else -> 
 				{:unknown_error, anything_else}
-		end
-	end
+    end
+  end
 
 
-	###################################################################
-	# (public)
-	#
 	# new!/0 - get the pid directly and not a status tuple
-	#
 
-	def new! do
-		case init() do
-			{:ok, pid} 
-				-> pid
-			{:error, error}
-				-> error
+  def new! do
+    case init() do
+      {:ok, pid} 
+        -> pid
+      {:error, error}
+        -> error
 		end
 	end
 
+  ############ Vertex functions ################################
 
-	###################################################################
-	# (public)
-	#
+  def vertex(graph_pid, key) do
+    Agent.get( graph_pid, fn graph -> :digraph.vertex(graph, key) end )
+  end
 
-	def vertices graph_pid do
-		Agent.get graph_pid, fn graph -> graph |> :digraph.vertices end
-	end
-
-
-	###################################################################
-	# (public)
-	#
-
-	def add_vertex graph_pid do
-		Agent.get graph_pid, fn graph -> graph |> :digraph.add_vertex end
-	end
-
-
-	###################################################################
-	# (public)
-	#
-
-	def add_vertex graph_pid, key do
-		Agent.get graph_pid, fn graph -> (graph |> :digraph.add_vertex key) end
-	end
-	
-
-	###################################################################
-	# (public)
-	#
-	
-	def add_vertex graph_pid, key, value do
-		Agent.get graph_pid, fn graph ->  (graph |> :digraph.add_vertex key, value) end
-	end
-	
-
-	###################################################################
-	# (public)
-	#
-	
-	def edges graph_pid do
-		Agent.get graph_pid, fn graph -> graph |> :digraph.edges end
-	end
-	
-
-	###################################################################
-	# (public)
-	#
-	
-	def add_edge graph_pid, edge_k, v1_k, v2_k, edge_v do
-		Agent.get( graph_pid, 
-			fn graph -> 
-        graph
-        |> :digraph.add_edge edge_k, v1_k, v2_k, edge_v
-			end
-		)
-	end
-	
-
-	###################################################################
-	# (public)
-	#
-	
-	def edge(graph_pid, key) do
-		Agent.get( graph_pid, fn graph -> :digraph.edge(graph, key) end )
-	end
-	
-
-	###################################################################
-	# (public)
-	#
-	
-	def vertex(graph_pid, key) do
-		Agent.get( graph_pid, fn graph -> :digraph.vertex(graph, key) end )
-	end
-
-
-  ###################################################################
-  # (public)
-  #
-  # notes - just returns the value, not the {:key, :value} tuple
-  
+  # notes - just returns the value, not the {:key, :value} tuple  
   def vertex!(graph_pid, key) do
     Agent.get( graph_pid, 
       fn graph ->
@@ -138,9 +52,51 @@ defmodule Graph do
     )
   end
 
+  def add_vertex graph_pid do
+    Agent.get graph_pid, fn graph -> graph |> :digraph.add_vertex end
+  end
+
+  def add_vertex graph_pid, key do
+    Agent.get graph_pid, fn graph -> (graph |> :digraph.add_vertex key) end
+	end
+	
+	def add_vertex graph_pid, key, value do
+		Agent.get graph_pid, fn graph ->  (graph |> :digraph.add_vertex key, value) end
+	end
+	
+  def vertices graph_pid do
+    Agent.get graph_pid, fn graph -> graph |> :digraph.vertices end
+  end
+
+  ############ Edge functions ##################################
+
+  def edge(graph_pid, key) do
+    Agent.get( graph_pid, fn graph -> :digraph.edge(graph, key) end )
+  end
+
+  def edge!(graph_pid, key) do
+    Agent.get( graph_pid, 
+      fn graph ->
+        case :digraph.edge(graph, key) do
+          {x, y} -> y
+          other_msg -> other_msg
+        end
+      end 
+    )
+  end
+	
+	def add_edge graph_pid, edge_k, v1_k, v2_k, edge_v do
+    Agent.get( graph_pid, fn graph -> :digraph.add_edge(graph, edge_k, v1_k, v2_k, edge_v) end)
+	end
+	
+  def edges graph_pid do
+    Agent.get graph_pid, fn graph -> :digraph.edges(graph) end
+  end
+
+
   ###################################################################
-  # (public)
-  #
+  # show/1
+  # 
   # notes - for graphical view of vertices/edges
   #         also debugging
   
